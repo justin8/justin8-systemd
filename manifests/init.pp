c8140a40632698defbcd6508cfbae5a6f30af153
Davide Ferrari <davide@trovit.com>
2015-11-25 17:04:13 +0100

    Fix if condition To prevent possible errors with puppet 3.x and facter versions, we rely on this stdlib function to force convertion to bool

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,14 +1,14 @@
 class systemd {
 
-  if $::systemd_available {
+  if ( str2bool("$::systemd_available") ) {
     exec { 'systemd-daemon-reload':
       path        => '/bin:/usr/bin:/sbin:/usr/sbin',
       command     => 'systemctl daemon-reload',
       refreshonly => true
     }
 
-   # refresh systemd before every service
-   Exec['systemd-daemon-reload'] -> Service<| |>
+    # refresh systemd before every service
+    Exec['systemd-daemon-reload'] -> Service<| |>
   }
 
   case $::osfamily {
