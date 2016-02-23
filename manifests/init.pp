56c50f194bd97395f661d36e90933f145ddb5d59
Alex Iribarren <Alex.Iribarren@cern.ch>
2016-02-23 17:02:30 +0100

    Change default paths

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,7 @@
+#
 class systemd {
 
-  if ( str2bool("$::systemd_available") ) {
+  if ( str2bool($::systemd_available) ) {
     exec { 'systemd-daemon-reload':
       path        => '/bin:/usr/bin:/sbin:/usr/sbin',
       command     => 'systemctl daemon-reload',
@@ -13,16 +14,16 @@ class systemd {
 
   case $::osfamily {
     'Archlinux': {
-      $unit_path = '/usr/lib/systemd/system'
+      $unit_path = '/etc/systemd/system'
     }
     'Debian': {
-      $unit_path = '/lib/systemd/system'
+      $unit_path = '/etc/systemd/system'
     }
     'RedHat': {
-      $unit_path = '/usr/lib/systemd/system'
+      $unit_path = '/etc/systemd/system'
     }
     default: {
-      $unit_path = '/usr/lib/systemd/system'
+      $unit_path = '/etc/systemd/system'
     }
   }
 
