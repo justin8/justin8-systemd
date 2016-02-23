b23681835d8eac869ab6bd0efe9cd770eb1a01e8
Alex Iribarren <Alex.Iribarren@cern.ch>
2016-02-23 16:32:30 +0100

    Added systemd::service

---
+++ manifests/service.pp
@@ -0,0 +1,32 @@
+#
+define systemd::service (
+    $execstart,
+    $servicename    = $name,
+    $description    = '',
+    $execstop       = undef,
+    $execreload     = undef,
+    $workingdir     = undef,
+    $restart        = 'always',
+    $restartsec     = undef,
+    $user           = 'root',
+    $group          = 'root',
+    $forking        = false,
+    $requires       = [],
+    $wants          = [],
+    $after          = [],
+    $wantedby       = ['multi-user.target'],
+) {
+
+    include systemd
+
+    validate_re($restart, ['^always$', '^no$', '^on-(success|failure|abnormal|abort|watchdog)$'], "Not a supported restart type: ${restart}")
+
+    file { "${::systemd::unit_path}/${servicename}.service":
+        ensure  => 'present',
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+        content => template("${module_name}/service.erb"),
+        notify  => Exec['systemd-daemon-reload'],
+    }
+}
